afd4d77f1827fd5d5782ce07cd437657b3823ffb
Xing.Tian <2360633699@qq.com>
2018-11-08 10:06:24 +0800

    Update install_android_sdk.sh

--- shell/install_android_sdk.sh
+++ shell/install_android_sdk.sh
@@ -11,16 +11,18 @@ SDK_NAME=sdk-tools-linux-${SDK_TOOLS_VERSION}.zip
 
 # download sdk tools
 wget http://dl.google.com/android/repository/${SDK_NAME}
+rm -rf ${ANDROID_HOME}
+mkdir -p ${ANDROID_HOME}
 unzip -qd ${ANDROID_HOME} ${SDK_NAME}
 rm -rf ${SDK_NAME}
 
+cd ${ANDROID_HOME}
+
 # update and install android platform ,tools
-(yes | ./${ANDROID_HOME}/tools/bin/sdkmanager --no_https --update)
+(yes | ./tools/bin/sdkmanager --no_https --update)
 (yes | ./tools/bin/sdkmanager --no_https --update) && \
 (yes | ./tools/bin/sdkmanager --no_https "build-tools;${BUILD_TOOLS_VERSION}") && \
 (yes | ./tools/bin/sdkmanager --no_https "platform-tools") && \
 (yes | ./tools/bin/sdkmanager --no_https "platforms;android-${COMPILE_SDK_VERSION}")
-
-echo -e "\n-------------------set android sdk env success!---------------"
 
 
